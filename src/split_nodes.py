1cb46b5bf7449d07ec64c21ab7126dd7dd55d9ea
m-hines <mahines9@gmail.com>
2026-09-20 12:19:55 -0400

    Updated split_nodes and text_to_textnodes

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -4,39 +4,43 @@ from textnode import TextNode, TextType
 def split_nodes_image(old_nodes):   # returns a list of TextNodes
     node_list = []
     for node in old_nodes:
-        original_text = node.text
-        split_image = extract_markdown_images(node.text)
-        for i in split_image:
-            delimiter = f"![{i[0]}]({i[1]})"
-            sections = original_text.split(delimiter, 1)
-            if sections[0] != "":
-                new_plain_node = TextNode(sections[0], TextType.PLAIN_TEXT)
+        if node.text_type != TextType.PLAIN_TEXT:
+            node_list.append(node)
+        else:
+            original_text = node.text
+            split_image = extract_markdown_images(node.text)
+            for i in split_image:
+                delimiter = f"![{i[0]}]({i[1]})"
+                sections = original_text.split(delimiter, 1)
+                if sections[0] != "":
+                    new_plain_node = TextNode(sections[0], TextType.PLAIN_TEXT)
+                    node_list.append(new_plain_node)
+                new_image_node = TextNode(i[0], TextType.IMAGE, i[1])
+                node_list.append(new_image_node)
+                original_text = sections[1]
+            if len(original_text) > 0:
+                new_plain_node = TextNode(original_text, TextType.PLAIN_TEXT)
                 node_list.append(new_plain_node)
-            new_image_node = TextNode(i[0], TextType.IMAGE, i[1])
-            node_list.append(new_image_node)
-            original_text = sections[1]
-        if len(original_text) > 0:
-            new_plain_node = TextNode(original_text, TextType.PLAIN_TEXT)
-            node_list.append(new_plain_node)
     return node_list
 
 def split_nodes_link(old_nodes):
     node_list = []
     for node in old_nodes:
-        original_text = node.text
-        split_link = extract_markdown_links(node.text)
-        for i in split_link:
-            delimiter = f"[{i[0]}]({i[1]})"
-            sections = original_text.split(delimiter, 1)
-            if sections[0] != "":
-                new_plain_node = TextNode(sections[0], TextType.PLAIN_TEXT)
+        if node.text_type != TextType.PLAIN_TEXT:
+            node_list.append(node)
+        else:
+            original_text = node.text
+            split_link = extract_markdown_links(node.text)
+            for i in split_link:
+                delimiter = f"[{i[0]}]({i[1]})"
+                sections = original_text.split(delimiter, 1)
+                if sections[0] != "":
+                    new_plain_node = TextNode(sections[0], TextType.PLAIN_TEXT)
+                    node_list.append(new_plain_node)
+                new_link_node = TextNode(i[0], TextType.LINK, i[1])
+                node_list.append(new_link_node)
+                original_text = sections[1]
+            if len(original_text) > 0:
+                new_plain_node = TextNode(original_text, TextType.PLAIN_TEXT)
                 node_list.append(new_plain_node)
-            new_link_node = TextNode(i[0], TextType.LINK, i[1])
-            node_list.append(new_link_node)
-            original_text = sections[1]
-        if len(original_text) > 0:
-            new_plain_node = TextNode(original_text, TextType.PLAIN_TEXT)
-            node_list.append(new_plain_node)
     return node_list
-
-# consider edge cases (see Boot's last comment in Split Images and Links)
--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -1,8 +1,12 @@
 from split_delimiter import split_nodes_delimiter
 from split_nodes import split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType
 
 def text_to_textnodes(text):
-    nodes_list = [TextNode(text, TextType.PLAIN_TEXT)]
-    nodes_list_split_delimiter = split_delimiter(textnodes_list)
-    nodes_list_split_nodes = split_nodes(nodes_list_split_delimiter)
-    return nodes_list_split_nodes
+    node = [TextNode(text, TextType.PLAIN_TEXT)]
+    node_split_first = split_nodes_delimiter(node, "**", TextType.BOLD_TEXT)
+    node_split_second = split_nodes_delimiter(node_split_first, "_", TextType.ITALIC_TEXT)
+    node_split_third = split_nodes_delimiter(node_split_second, "`", TextType.CODE_TEXT)
+    node_split_fourth = split_nodes_image(node_split_third)
+    node_split_fifth = split_nodes_link(node_split_fourth)
+    return node_split_fifth
